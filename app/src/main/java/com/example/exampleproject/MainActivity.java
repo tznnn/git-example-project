c18c1ca1f0e192462283f22c5d9cc01a0ab595ae
atakan tuzun <tuzun185@gmail.com>
2021-11-01 14:09:40 +0300

    Import butterknife library.

--- app/src/main/java/com/example/exampleproject/MainActivity.java
+++ app/src/main/java/com/example/exampleproject/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.exampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.textView)
+    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tx.setText("atakan");
     }
 }
